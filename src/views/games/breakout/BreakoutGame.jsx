b0c521dc2aed0a6d911c4e59a6ca51d3f060370b
Lucas Moniz <lucas@yourcabana.com>
2026-05-10 21:00:45 -0300

    .

--- src/views/games/breakout/BreakoutGame.jsx
+++ src/views/games/breakout/BreakoutGame.jsx
@@ -115,7 +115,7 @@ export default function BreakoutGame() {
       <ControlsLegend controls={[
         ['← / A', 'move'],
         ['→ / D', 'move'],
-        ['CLICK / SPACE', 'jump'],
+        ['CLICK / SPACE', 'move-paddle'],
       ]} />
     </div>
   );
